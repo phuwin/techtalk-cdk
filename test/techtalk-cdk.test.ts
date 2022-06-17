cbc5bf1023886af67acba7e245eceef3115b5b2f
Phu Nguyen <ngp130895@gmail.com>
2022-06-17 13:13:15 +0300

    Initial commit

---
+++ test/techtalk-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechtalkCdk from '../lib/techtalk-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/techtalk-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechtalkCdk.TechtalkCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
