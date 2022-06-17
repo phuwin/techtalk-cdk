ad48765d7abe3362e77634941c435936a90f0633
Phu Nguyen <ngp130895@gmail.com>
2022-06-17 13:45:05 +0300

    feat: added logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export async function main(event : any) {
   console.log('event 👉', event);
+  console.log('i am run')
 
   return {
     body: JSON.stringify({message: 'Successful lambda invocation'}),
